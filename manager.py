4a053e189ba5a484a9862d6767fbd703ff6b4f8e
au9ustine <duke.augustine@gmail.com>
2019-06-24 18:03:35 +0800

    change sprint calculation after 2019

--- manager.py
+++ manager.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 """Sprint Date Manager
-A program to manage sprint date
+A tiny tool to manage sprint date
 
-Copyright (c) 2015 Copyright Shao Tian-Chen (Austin) All Rights Reserved.
+Copyright (c) 2015-2019 Copyright Shao Tianchen
+All Rights Reserved.
 """
 
 from __future__ import absolute_import
@@ -13,22 +14,19 @@ from datetime import date
 from itertools import count
 from itertools import islice
 import datetime
-import logging
-import re
-import sys
+import argparse
 
-VERSION = (1, 0, 0)
+VERSION = (1, 1, 0)
 
 
 class Sprint(object):
     """Sprint entity
     """
-    def __init__(self, GENESIS_DAY=None, SPRINT_LEN=None):
+
+    def __init__(self, GENESIS_DAY, SPRINT_LEN):
         """Initialized by GENESIS_DAY and SPRINT_LEN"""
-        if GENESIS_DAY is None:
-            self.GENESIS_DAY = datetime.date(2013, 12, 21)
-        if SPRINT_LEN is None:
-            self.SPRINT_LEN = datetime.timedelta(days=14)
+        self.GENESIS_DAY = GENESIS_DAY
+        self.SPRINT_LEN = SPRINT_LEN
         assert isinstance(self.GENESIS_DAY, datetime.date)
         assert isinstance(self.SPRINT_LEN, datetime.timedelta)
         assert self.SPRINT_LEN.days > 0
@@ -38,16 +36,20 @@ class Sprint(object):
         assert isinstance(sprint_num, int)
         if sprint_num < 0:
             sprint_num = 0
-        default_case = (0, self.GENESIS_DAY, self.GENESIS_DAY + datetime.timedelta(days=self.SPRINT_LEN.days-1))
-        i, start_date, end_date = next(islice(self.get_sprints(), sprint_num, None), default_case)
+        default_case = (0, self.GENESIS_DAY, self.GENESIS_DAY +
+                        datetime.timedelta(days=self.SPRINT_LEN.days-1))
+        i, start_date, end_date = next(
+            islice(self.get_sprints(), sprint_num, None), default_case)
         return (i, start_date, end_date)
 
     def get_sprints(self):
         """Given genesis dayx, sprint len l, function f, when f(x, l), output lazy evalutaion of all sprints"""
         return ((i,
-            self.GENESIS_DAY + datetime.timedelta(days=self.SPRINT_LEN.days*i),
-            self.GENESIS_DAY + datetime.timedelta(days=self.SPRINT_LEN.days*(i+1)-1)
-        ) for i in count())
+                 self.GENESIS_DAY +
+                 datetime.timedelta(days=self.SPRINT_LEN.days*i),
+                 self.GENESIS_DAY +
+                 datetime.timedelta(days=self.SPRINT_LEN.days*(i+1)-1)
+                 ) for i in count())
 
     def get_sprint(self, any_date):
         if (any_date - self.GENESIS_DAY).days < 0:
@@ -55,6 +57,17 @@ class Sprint(object):
         for i, start_date, end_date in self.get_sprints():
             if abs((any_date - start_date).days) < self.SPRINT_LEN.days:
                 return (i, start_date, end_date)
+
+    def __str__(self):
+        i: int
+        start_date: datetime.date
+        end_date: datetime.date
+        i, start_date, end_date = self.get_sprint(date.today())
+        return str((
+            i,
+            start_date.isoformat(),
+            end_date.isoformat()
+        ))
 
     def get_sprint_number(self, any_date):
         """Given date x and function f, when f(x), then output sprint number"""
@@ -79,27 +92,23 @@ class Sprint(object):
         return end_date
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        s = Sprint()
-        a, b, c = s.get_sprint(date.today())
-        print((a, str(b), str(c)))
-    elif len(sys.argv) == 2:
-        s = Sprint()
-        x = sys.argv[1]
-        pattern = r'^(\d+)-(\d{2})-(\d{2})$'
-        if re.match(r'^[-]?\d+$', x):
-            a, b, c = s.get_date(int(x))
-            print((a, str(b), str(c)))
-        elif re.match(pattern, x):
-            yyyy, mm, dd = re.findall(pattern, x)[0]
-            try:
-                x_date = date(int(yyyy), int(mm), int(dd))
-                a, b, c = s.get_sprint(x_date)
-                print((a, str(b), str(c)))
-            except:
-                print('''See, you lost a date ... ''')
-        else:
-            print('''Hey, you need to know how to count or date ;) ''')
-    else:
-        print('''Hey, you need to know how to count or date ;) ''')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--genesis-day', type=str, default='2013-12-21')
+    parser.add_argument('--sprint-len', type=int, default=14)
+    parser.add_argument('--version', action='version',
+                        version='v' + '.'.join(map(str, VERSION)))
+    args = parser.parse_args()
+    genesis_day = datetime.date.fromisoformat(args.genesis_day)
+    if datetime.date.today().year > 2019:
+        # 2019-W26-1: Calibrate sprints to align ISO weekdays
+        genesis_day += datetime.timedelta(days=2)
+    sprint_len = datetime.timedelta(days=args.sprint_len)
+    s = Sprint(
+        genesis_day,
+        sprint_len
+    )
+    print(str(s))
+
+
+main()
